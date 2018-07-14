0b067b2c2fa31d90a4e37ed2a8288437c04c7ae5
andyxmq <andyxmq@126.com>
2018-07-14 17:16:07 +0800

    v0.0.1初次提交

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("aa");
+class FastCache {
+    constructor() {
+        this.list = {};
+    }
+    set(key, value){
+        this.list[key] = value;
+    }
+    get(key) {
+        return this.list[key];
+    }
+}
+
+window.FastCache = FastCache;
